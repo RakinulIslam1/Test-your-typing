d308b04f1e838f0bd7f164700e3ff715741c7c60
راكينول إسلام <you@example.com>
2022-09-15 00:12:01 +0600

    d-none done

--- script.js
+++ script.js
@@ -112,12 +112,15 @@ const start = () => {
 
     // finished timer
     if (count == 0) {
+
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
+    
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
+      
       startTime = new Date().getTime();
     }
     count--;
