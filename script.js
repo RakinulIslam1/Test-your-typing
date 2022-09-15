c1ce759aa4b3fa1957b745bb8c4c8522bb368e7e
راكينول إسلام <you@example.com>
2022-09-15 09:34:52 +0600

    modal done

--- script.js
+++ script.js
@@ -138,6 +138,5 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
   document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
